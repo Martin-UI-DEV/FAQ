c8bf09980543705aca9e0a25fe3ef5f077ddf8d7
martin gomez <mgomez@syncromind.com>
2024-10-08 15:37:19 -0300

    Fix animation

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
-const accordion = [...$$('.accordion')];
-const items = [...$$('.accordion-item')];
 const btns = [...$$('.accordion-header')];
-const bodies = [...$$('.accordion-body')];
-
 
 btns.forEach( btn => {
-    btn.addEventListener('click', () => {
-        const parent = btn.parentNode;        
-        const itemActive = $('.accordion-item.show');
-        itemActive?.classList.remove('show');  
-        parent.classList.add('show');
-        items.forEach( item => {
-            const span = item.querySelector('span');
-            item.classList.contains('show') ? span.innerText ='-': span.innerText ='+';
-            
-        })
-    })
+    btn.addEventListener('click', () => {accordionToggle(btn)} )
 });
 
+function accordionToggle(btn) {
+    const parent = btn.parentNode;
+    const itemActive = $('.accordion-item.show');
+    if (itemActive && itemActive !== parent) {
+        itemActive.classList.remove('show');
+    }
+    parent.classList.toggle('show');
+    changeIcon()
+}
 
+function changeIcon() {
+    const items = [...$$('.accordion-item')];
+    items.forEach(item => {
+        const span = item.querySelector('span');
+        item.classList.contains('show') ? span.innerText = '-' : span.innerText = '+';
+
+    })
+}
